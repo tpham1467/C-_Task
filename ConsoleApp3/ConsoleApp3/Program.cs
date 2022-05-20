183cf4a118ba3f6618e003918bab71fe5d5d807c
Tpham <Tpham@users.noreply.github.com>
2022-05-20 16:44:06 +0700

    tpham1467

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Net;
+using System.Text;
+
+class Program
+{
+    static   void Handel(HttpListenerContext  context)
+    {
+        Task t = new Task(async () =>
+        {
+            var respon = context.Response;
+            var outputstream = respon.OutputStream;
+            respon.Headers.Add("Content-Type", "text-html");
+            var html = " <h1> Chaop Ban</h1>";
+            var bytearr = Encoding.UTF8.GetBytes(html);
+            outputstream.Write(bytearr, 0, bytearr.Length);
+            outputstream.Close();
+            await Task.Delay(10000);
+            Console.WriteLine("Da Xong");
+        });
+        t.Start();
+        
+    }
+    static async Task Main(string[] args)
+    {
+        var server = new HttpListener();
+        server.Prefixes.Add("http://+:80/");
+
+        server.Start();
+        do
+        {
+            var context = await server.GetContextAsync();
+            Console.WriteLine("Have Client connect !!");
+            Task t = new Task(() =>
+            {
+                Handel(context);
+            }
+            );
+            t.Start();
+            await t;
+
+        } while (server.IsListening); 
+
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Text;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        while (true)
+        {
+            Task t = new Task(async () =>
+            {
+                using var httpClient = new HttpClient();
+                string url = @"http://localhost:80";
+                // Thiết lập các Header nếu cần
+                httpClient.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml+json");
+
+                // Thực hiện truy vấn GET
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+        string htmltext = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(htmltext);
+    });
+        t.Start();
+            await t;
+       await Task.Delay(10);
+
+        }
+    }
+}
