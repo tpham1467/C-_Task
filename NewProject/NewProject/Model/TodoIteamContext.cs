252cd078e4dd99b479a0b8873e49ea223f64c096
Tpham <Tpham@users.noreply.github.com>
2022-05-23 11:54:08 +0700

    tpham1467@gmail.com

---
+++ NewProject/NewProject/Model/TodoIteamContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+namespace NewProject.Model
+{using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+    public class TodoIteamContext : DbContext 
+    {
+        public TodoIteamContext(DbContextOptions<TodoIteamContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
